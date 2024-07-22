4b2dce9554d0b7c2f186d18e8312396fe59160fd
StephanZager <Stephan-Zager@web.de>
2024-07-22 19:59:50 +0200

    walking character

--- models/character.class.js
+++ models/character.class.js
@@ -2,7 +2,7 @@ class Character extends MovableObject {
 
     y = 180;
     height = 250;
-
+    speed = 5;
     IMAGES_WALK = [
         'img/2_character_pepe/2_walk/W-21.png',
         'img/2_character_pepe/2_walk/W-22.png',
@@ -11,8 +11,8 @@ class Character extends MovableObject {
         'img/2_character_pepe/2_walk/W-25.png',
         'img/2_character_pepe/2_walk/W-26.png'
     ]
+    world;
     
-
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
         this.loadImages(this.IMAGES_WALK);
@@ -21,15 +21,36 @@ class Character extends MovableObject {
     }
 
     animate() {
+
         setInterval(() => {
-            let i = this.currentImage % this.IMAGES_WALK.length;
-            let path = this.IMAGES_WALK[i];
-            this.img = this.imageCache[path];
-            this.currentImage++;
-        }, 100);
+            if (this.world.keyboard.RIGHT){
+                this.x += this.speed;
+                this.otherDirection = false;
+            }
+
+            if (this.world.keyboard.LEFT){
+                this.x -= this.speed;
+                this.otherDirection = true;
+            }
+        }, 1000 / 60);
+
+        setInterval(() => {
+
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+
+                let i = this.currentImage % this.IMAGES_WALK.length;
+                let path = this.IMAGES_WALK[i];
+                this.img = this.imageCache[path];
+                this.currentImage++;
+            }
+        }, 50);
     }
 
-    jump() {
 
-    }
+
+
+
+jump() {
+
+}
 }
