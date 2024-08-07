bef3da7fddec2d77b8cdb353c5ca4b8cfa47a7d1
StephanZager <Stephan-Zager@web.de>
2024-08-07 20:25:24 +0200

    bottle height and width fixed

--- models/bottle.class.js
+++ models/bottle.class.js
@@ -1,8 +1,17 @@
 class Bottle extends MovableObject{
 
+    height = 100;
+    width = 80;
+
     constructor(imagePath, x, y){
         super().loadImage(imagePath);
         this.x = 200 + Math.random() * 1900;
         this.y = y;
+        this.offset = {
+            top: 10,
+            bottom: 10,
+            left: 30,
+            right: 30,
+        }
     }
 }
--- models/drawable-object.class.js
+++ models/drawable-object.class.js
@@ -31,7 +31,7 @@ class DrawableObject {
 
     drawFrame(ctx) {
 
-        if (this instanceof Character || this instanceof Chicken || this instanceof Coin) {
+        if (this instanceof Character || this instanceof Chicken || this instanceof Coin || this instanceof Bottle) {
             ctx.beginPath();
             ctx.lineWidth = '5';
             ctx.strokeStyle = 'blue';
