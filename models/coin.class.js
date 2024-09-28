72a5eb1a53d8cc3c9f8c002915e3edbada162767
StephanZager <Stephan-Zager@web.de>
2024-09-28 19:09:16 +0200

    coinbar jsdoch commit

--- models/coin.class.js
+++ models/coin.class.js
@@ -1,7 +1,17 @@
+/**
+ * Represents a coin in the game.
+ * 
+ */
 class Coin extends MovableObject {
+    
     y = 100; 
           
-
+    /**
+     * Creates an instance of Coin.
+     * Initializes the coin with a given x-coordinate and random y-coordinate.
+     * @param {number} x - The x-coordinate of the coin.
+     * 
+     */
     constructor(x){
         super().loadImage('img/8_coin/coin_1.png');
         this.x = x;
