c67c05f4a7deeaaf79877ecbc2902746ada813b8
StephanZager <Stephan-Zager@web.de>
2024-09-28 19:44:05 +0200

    jsdoc class world

--- models/world.class.js
+++ models/world.class.js
@@ -1,77 +1,121 @@
+/**
+ * Represents the game world.
+ * 
+ */
 class World {
 
     character = new Character();
     boss = new Endboss();
     level = level1;
     canvas;
-    ctx;
-    keyboard;
-    camera_x = 0;
     statusBar = new StatusBar();
     coinBar = new StatusCoinBar();
     bottleBar = new StatusBottleBar();
     throwableObject = [];
+    keyboard;
 
+    /**
+     * The canvas rendering context.
+     * @type {CanvasRenderingContext2D}
+     */
+    ctx;
 
+    /**
+     * The x-coordinate of the camera.
+     * @type {number}
+     */
+    camera_x = 0;
 
+    /**
+     * Creates an instance of World.
+     * @param {HTMLCanvasElement} canvas - The canvas element to render the game.
+     * 
+     */
     constructor(canvas) {
-
         this.ctx = canvas.getContext('2d');
         this.canvas = canvas;
         this.keyboard = keyboard;
         this.draw();
         this.setWorld();
         this.run();
-    }
+    };
 
-
-
+    /**
+     * Sets the world reference for the character.
+     * 
+     */
     setWorld() {
         this.character.world = this;
-    }
+    };
 
+    /**
+     * Runs the game loop, checking for collisions and other game logic.
+     * 
+     */
     run() {
-
         setInterval(() => {
             this.checkCollision();
-            this.checkThrowObject();
-            this.checkCoinObject();
-            this.checkBottleObject();
         }, 100);
-    }
+    };
 
+    /**
+     * Checks for various types of collisions in the game.
+     * 
+     */
+    checkCollision() {
+        this.checkCharacterEnemyCollision();
+        this.checkCharacterJumpingCollision();
+        this.checkThrowableObjectCollision();
+        this.updateCharacterStatusBar();
+        this.checkThrowObject();
+        this.checkCoinObject();
+        this.checkBottleObject();
+    };
+
+    /**
+     * Checks for collisions between the character and coins.
+     * 
+     */
     checkCoinObject() {
         this.level.coin.forEach((coin, index) => {
             if (this.character.isColliding(coin)) {
-
-
                 playSound('coin', 'collect');
-
-                this.coinBar.collectCoin(index, this.level.coin);  // hier überge ich this. .. weil cih dann nicht drauf zuggeifen kann                                                                               
+                this.coinBar.collectCoin(index, this.level.coin);
             }
         });
-    }
+    };
 
+    /**
+    * Checks for collisions between the character and bottles.
+    * 
+    */
     checkBottleObject() {
         this.level.bottle.forEach((bottle, index) => {
             if (this.character.isColliding(bottle)) {
                 playSound('bottle', 'collect');
-                this.bottleBar.collectBottle(index, this.level.bottle);  // hier überge ich this. .. weil cih dann nicht drauf zuggeifen kann                                                                               
+                this.bottleBar.collectBottle(index, this.level.bottle);
             }
         });
-    }
+    };
 
+    /**
+     * Checks if the character throws a bottle.
+     * 
+     */
     checkThrowObject() {
         if (this.keyboard.D && this.bottleBar.bottles > 0) {
             let bottle = new ThrowableObject(this.character.x + 100, this.character.y + 100);
             playSound('bottle', 'throw');
             this.throwableObject.push(bottle);
-            this.bottleBar.setPercentage(this.bottleBar.bottles - 1); // bootle bar ist die classe dann kann ich da auch hin ;) weil oben ich das definiere
+            this.bottleBar.setPercentage(this.bottleBar.bottles - 1);
+        };
+    };
 
-        }
-    }
-
-    checkCollision() {
+    /**
+     * Checks for collisions between the character and enemies.
+     * 
+     */
+    checkCharacterEnemyCollision() {
         this.level.enemies.forEach((enemy) => {
             if (this.character.isColliding(enemy)) {
                 if (enemy instanceof Endboss) {
@@ -88,7 +132,13 @@ class World {
                 }
             }
         });
+    };
 
+    /**
+     * Checks for collisions between the character and enemies when jumping.
+     * 
+     */
+    checkCharacterJumpingCollision() {
         this.level.enemies.forEach((enemy) => {
             if (this.character.isCollidingJumping(enemy)) {
                 if (enemy instanceof Chicken || enemy instanceof SmallChicken) {
@@ -100,7 +150,13 @@ class World {
                 }
             }
         });
+    };
 
+    /**
+     * Checks for collisions between throwable objects and enemies.
+     * 
+     */
+    checkThrowableObjectCollision() {
         this.throwableObject.forEach((bottle, bottleIndex) => {
             this.level.enemies.forEach((enemy, index) => {
                 if (bottle.isColliding(enemy)) {
@@ -115,18 +171,25 @@ class World {
                 }
             });
         });
+    };
 
+    /**
+     * Updates the character's status bar based on collisions with enemies.
+     * 
+     */
+    updateCharacterStatusBar() {
         this.level.enemies.forEach((enemy) => {
             if (this.character.isColliding(enemy)) {
                 this.character.hit();
                 this.statusBar.setPercentage(this.character.energy);
-               
             }
         });
-    }
+    };
 
-
-
+    /**
+     * Draws the game world on the canvas.
+     * 
+     */
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.ctx.translate(this.camera_x, 0);
@@ -148,36 +211,56 @@ class World {
         requestAnimationFrame(function () {
             self.draw();
         });
-    }
+    };
 
+    /**
+     * Adds an array of objects to the map.
+     * @param {Array} objects - The objects to add to the map.
+     * 
+     */
     addObjectToMap(objects) {
         objects.forEach(o => {
             this.addToMap(o);
         })
-    }
+    };
 
+    /**
+     * Adds a single object to the map.
+     * @param {Object} mo - The object to add to the map.
+     * 
+     */
     addToMap(mo) {
         if (mo.otherDirection) {
             this.flipImage(mo);
-        }
+        };
 
         mo.draw(this.ctx);
         mo.drawFrame(this.ctx);
 
         if (mo.otherDirection) {
             this.flipImageBack(mo);
-        }
-    }
+        };
+    };
 
+    /**
+     * Flips an image horizontally.
+     * @param {Object} mo - The object to flip.
+     * 
+     */
     flipImage(mo) {
         this.ctx.save();
         this.ctx.translate(mo.width, 0);
         this.ctx.scale(-1, 1);
         mo.x = mo.x * -1;
-    }
+    };
 
+    /**
+     * Flips an image back to its original orientation.
+     * @param {Object} mo - The object to flip back.
+     * 
+     */
     flipImageBack(mo) {
         mo.x = mo.x * -1;
         this.ctx.restore();
-    }
+    };
 }
