3b7608283460577f76e11ff150b79f76e0184522
StephanZager <Stephan-Zager@web.de>
2024-09-28 17:54:35 +0200

    clouds commit jsdoc

--- models/clouds.class.js
+++ models/clouds.class.js
@@ -1,18 +1,33 @@
+/**
+ * Represents the clouds in the game.
+ * 
+ */
 class Clouds extends MovableObject {
+
     y = 30;
     height = 400;
     width = 700;
-    
+
+    /**
+     * Creates an instance of Clouds.
+     * Initializes the clouds with a given x-coordinate and starts the animation.
+     * 
+     * @param {number} x - The x-coordinate of the clouds.
+     * 
+     */
     constructor(x) {
         super().loadImage('img/5_background/layers/4_clouds/1.png');
         this.x = x;
         this.animate();
     }
 
-    animate() { 
-        setInterval(() => {           
+    /**
+     * Animates the clouds by moving them to the left.
+     * 
+     */
+    animate() {
+        setInterval(() => {
             this.moveLeft();
         }, 1000 / 60);
-        
-    }    
+    }
 }
